4f81d99a6b19521d41061d7c55060420b3b50d8f
Floglor <zaric-igor@ya.ru>
2017-08-27 16:21:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,185 @@
+class Main {
+
+    // public void Roll3d6Stats() {
+    //   Random rand = new Random();
+    //   int a1 = 0;
+    //   int a2 = 0;
+    //   int a3 = 0;
+    //   int a4 = 0;
+    //   int a5 = 0;
+    //   int a6 = 0;
+    //   int a7 = 0;
+    //   int a8 = 0;
+    //   int a9 = 0;
+    //   int a10 = 0;
+    //   int a11 = 0;
+    //   int a12 = 0;
+    //   int a13 = 0;
+    //   int a14 = 0;
+    //   int a15 = 0;
+    //   int a16 = 0;
+    //   int a17 = 0;
+    //   int a18 = 0;
+//
+    //   int firstroll;
+    //   int secondroll;
+    //   int thirdroll;
+    //   int rollresult;
+//
+    //   for (int i = 0; i < 100; i++) {
+    //       firstroll = rand.nextInt(6) + 1;
+    //       secondroll = rand.nextInt(6) + 1;
+    //       thirdroll = rand.nextInt(6) + 1;
+    //       rollresult = firstroll + secondroll + thirdroll;
+//
+    //       switch (rollresult){
+    //           case 1: { a1++; break; }
+    //           case 2: { a2++; break; }
+    //           case 3: { a3++; break; }
+    //           case 4: { a4++; break; }
+    //           case 5: { a5++; break; }
+    //           case 6: { a6++; break; }
+    //           case 7: { a7++; break; }
+    //           case 8: { a8++; break; }
+    //           case 9: { a9++; break; }
+    //           case 10: { a10++; break; }
+    //           case 11: { a11++; break; }
+    //           case 12: { a12++; break; }
+    //           case 13: { a13++; break; }
+    //           case 14: { a14++; break; }
+    //           case 15: { a15++; break; }
+    //           case 16: { a16++; break; }
+    //           case 17: { a17++; break; }
+    //           case 18: { a18++; break; }
+//
+    //       }
+//
+    //   }
+//
+    //   System.out.println("a1 = " + a1);
+    //   System.out.println("a2 = " + a2);
+    //   System.out.println("a3 = " + a3);
+    //   System.out.println("a4 = " + a4);
+    //   System.out.println("a5 = " + a5);
+    //   System.out.println("a6 = " + a6);
+    //   System.out.println("a7 = " + a7);
+    //   System.out.println("a8 = " + a8);
+    //   System.out.println("a9 = " + a9);
+    //   System.out.println("a10 = " + a10);
+    //   System.out.println("a11 = " + a11);
+    //   System.out.println("a12 = " + a12);
+    //   System.out.println("a13 = " + a13);
+    //   System.out.println("a14 = " + a14);
+    //   System.out.println("a15 = " + a15);
+    //   System.out.println("a16 = " + a16);
+    //   System.out.println("a17 = " + a17);
+    //   System.out.println("a18 = " + a18);
+//    }
+
+    public static void main(String[] args) {
+
+        //   Random rand = new Random();
+        //   int a1 = 0;
+        //   int a2 = 0;
+        //   int a3 = 0;
+        //   int a4 = 0;
+        //   int a5 = 0;
+        //   int a6 = 0;
+        //   int a7 = 0;
+        //   int a8 = 0;
+        //   int a9 = 0;
+        //   int a10 = 0;
+        //   int a11 = 0;
+        //   int a12 = 0;
+        //   int a13 = 0;
+        //   int a14 = 0;
+        //   int a15 = 0;
+        //   int a16 = 0;
+        //   int a17 = 0;
+        //   int a18 = 0;
+//
+        //   int firstroll;
+        //   int secondroll;
+        //   int thirdroll;
+        //   int rollresult;
+//
+        //   for (int i = 0; i < 100000000; i++) {
+        //       firstroll = rand.nextInt(6) + 1;
+        //       secondroll = rand.nextInt(6) + 1;
+        //       thirdroll = rand.nextInt(6) + 1;
+        //       rollresult = firstroll + secondroll + thirdroll;
+//
+        //       switch (rollresult){
+        //           case 1: { a1++; break; }
+        //           case 2: { a2++; break; }
+        //           case 3: { a3++; break; }
+        //           case 4: { a4++; break; }
+        //           case 5: { a5++; break; }
+        //           case 6: { a6++; break; }
+        //           case 7: { a7++; break; }
+        //           case 8: { a8++; break; }
+        //           case 9: { a9++; break; }
+        //           case 10: { a10++; break; }
+        //           case 11: { a11++; break; }
+        //           case 12: { a12++; break; }
+        //           case 13: { a13++; break; }
+        //           case 14: { a14++; break; }
+        //           case 15: { a15++; break; }
+        //           case 16: { a16++; break; }
+        //           case 17: { a17++; break; }
+        //           case 18: { a18++; break; }
+//
+        //       }
+//
+        //   }
+//
+        //   System.out.println("a1 = " + a1);
+        //   System.out.println("a2 = " + a2);
+        //   System.out.println("a3 = " + a3);
+        //   System.out.println("a4 = " + a4);
+        //   System.out.println("a5 = " + a5);
+        //   System.out.println("a6 = " + a6);
+        //   System.out.println("a7 = " + a7);
+        //   System.out.println("a8 = " + a8);
+        //   System.out.println("a9 = " + a9);
+        //   System.out.println("a10 = " + a10);
+        //   System.out.println("a11 = " + a11);
+        //   System.out.println("a12 = " + a12);
+        //   System.out.println("a13 = " + a13);
+        //   System.out.println("a14 = " + a14);
+        //   System.out.println("a15 = " + a15);
+        //   System.out.println("a16 = " + a16);
+        //   System.out.println("a17 = " + a17);
+        //   System.out.println("a18 = " + a18);
+        Character Reven = new Character(13, 12, 14, 9, "Reven");
+        CharWeapon Sabre = new CharWeapon("Sabre", true, 3, "SabreSkill");
+        Reven.setWeapon(Sabre);
+
+        Reven.AddArmor(new CharArmor("Armor Plate", "Torso", 3));
+
+        System.out.println("Reven uses Armor Plate, Sabre+3 and Shield+1");
+
+        Character Pleb = new Character(10, 10, 10, 10, "Pleb");
+        CharWeapon SabrePleb = new CharWeapon("Sabre", true, 3, "SabreSkill");
+        Pleb.setWeapon(SabrePleb);
+
+        Pleb.AddArmor(new CharArmor("Armor Plate", "Torso", 3));
+
+        System.out.println("Pleb uses Armor Plate+3, Sabre+3 and Shield+1");
+
+
+        while (Reven.isAlive() && Pleb.isAlive()) {
+            Reven.AttackChoice("Torso", Pleb);
+            if (!Pleb.isAlive()) break;
+            Pleb.AttackChoice("Torso", Reven);
+        }
+
+
+        //  CharSkill skill = new CharSkill("Sabre", "A", "DX", 24);
+        //  if (skill.getLvl() >= 0)
+        //      System.out.println(skill.getMainAttribute() + " + " + skill.getLvl());
+        //  else System.out.println(skill.getMainAttribute() + skill.getLvl());
+
+
+    }
+}
